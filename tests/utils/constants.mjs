06198e2ec3d3010be5a5153c0833e3cbcdcb3f5a
w-duffy <w.duffy@outlook.com>
2024-09-25 16:28:50 -0500

    Initial commit

---
+++ tests/utils/constants.mjs
@@ -0,0 +1,10 @@
+/*
+
+REMEMBER: Wake up Render before running `npm test`
+
+The below live site is a dummy site for testing:
+It contains correct and incorrect responses, so you can see errors and success messages.
+
+*/
+
+export const apiBaseUrl = "https://testing-action.onrender.com/api";
